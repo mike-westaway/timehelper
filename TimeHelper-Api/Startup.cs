cbf865e1c25c87aa86c71478bd220d880bc6bb11
nhill <nhill@MININT-CU56C8E>
2021-01-11 16:03:55 +0000

    attemot to force redeploy of api application

--- TimeHelper-Api/Startup.cs
+++ TimeHelper-Api/Startup.cs
@@ -24,7 +24,7 @@ namespace TimeHelper.Api
         {
             Configuration = configuration;
         }
-
+        // Test path trigger on github workflow
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
